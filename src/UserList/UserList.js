30d516eb920ea0ffbcfeae995697e606a00070b9
agata okolska <agata.okolska@gmail.com>
2018-08-08 11:32:50 +0200

    add objectentries and map, move function to utils component

--- src/UserList/UserList.js
+++ src/UserList/UserList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Default from './Default'
 import Loading from './Loading';
 import List from './List'
-
+import { mapObjectToArray } from '../utils.js'
 
 class UserList extends React.Component {
   state = {
@@ -17,12 +17,14 @@ class UserList extends React.Component {
       isLoadingUsers: true
     })
 
-    fetch('https://fir-sandbox-65a96.firebaseio.com/.json')
+    fetch('https://fir-sandbox-65a96.firebaseio.com/jfddl5-users.json')
       .then(response => response.json())
-      .then(data => this.setState({
-        users: data,
-        isLoadingUsers: false
-      }))
+      .then(data => {
+        this.setState({
+          users: mapObjectToArray(data),        // users: Object.entries(data || {}).map(item => (
+          isLoadingUsers: false             // { id: item[0], ...item[1] })), zamiast tego mamy mapObjectToArray z utils.js
+        })
+      })
   }
 
   render() {
