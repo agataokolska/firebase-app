4ce77d68f34800caafd1e76632dbad5f239e6cdf
agata okolska <agata.okolska@gmail.com>
2018-08-09 14:11:50 +0200

    add Auth component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import UserList from './UserList';
-
+import Auth from './Auth'
 
 class App extends Component {
   render() {
     return (
       <div>
-       <UserList/>
-         
+        <Auth>
+          <UserList />
+        </Auth>
       </div>
     );
   }
---
+++ src/Auth/Auth.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Auth = (props) => (
+    <div>
+        {props.children}
+    </div>
+)
+
+export default Auth
