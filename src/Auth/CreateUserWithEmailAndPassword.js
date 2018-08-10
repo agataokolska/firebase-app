65e8240de085e9192530003f99dcacccd9bb9245
agata okolska <agata.okolska@gmail.com>
2018-08-10 10:46:19 +0200

    add component CreateUserWithEmailAndPassword

---
+++ src/Auth/CreateUserWithEmailAndPassword.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+const CreateUserWithEmailAndPassword = (props) => (
+    <div>
+        <div>
+            <input
+                type="email"
+                onChange={props.onEmailChangedHandler}
+                value={props.emailValue}
+            />
+        </div>
+        <div>
+            <input
+                type="password"
+                onChange={props.onPasswordChangedHandler}
+                value={props.passwordValue}
+            />
+        </div>
+        <div>
+            <button
+                onClick={props.onLoginClickHandler}
+            >
+            Zaloguj!
+            </button>
+        </div>
+    </div>
+)
+
+export default CreateUserWithEmailAndPassword
--- src/Auth/LogInForms.js
+++ src/Auth/LogInForms.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import LoginByGoogle from './LoginByGoogle';
 import LoginByEmailAndPassword from './LoginByEmailAndPassword'
-
+import CreateUserWithEmailAndPassword from './CreateUserWithEmailAndPassword'
 const LogInForms = (props) => (
     <div>
         <LoginByGoogle
             onLoginClickHandler={props.onLoginByGoogleClickHandler}
         />
         <LoginByEmailAndPassword
+            emailValue={props.emailValue}
+            passwordValue={props.passwordValue}
+            onEmailChangedHandler={props.onEmailChangedHandler}
+            onPasswordChangedHandler={props.onPasswordChangedHandler}
+            onLoginClickHandler={props.onLoginByEmailClickHandler}
+        />
+        <CreateUserWithEmailAndPassword
             emailValue={props.emailValue}
             passwordValue={props.passwordValue}
             onEmailChangedHandler={props.onEmailChangedHandler}
